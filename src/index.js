7f10f0b03b4c3c15c3e51984f48900f14f3141c8
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-17 15:16:48 +0200

    feat: add part-working function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let str1 = str.split("");
+  for (let i = 0; i < str1.length; i++) {
+    if (
+      str1[i] != "[" &&
+      str1[i] != "{" &&
+      str1[i] != "}" &&
+      str1[i] != "]" &&
+      str1[i] != ")" &&
+      str1[i] != "(" &&
+      str1[i] != "|"
+    ) {
+      return true;
+    }
+  }
+};
