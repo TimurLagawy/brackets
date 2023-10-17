25577307933bad3adea4016ea9bd5c59dcc16ced
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-17 18:39:25 +0200

    feat: add code, who pass more tests

--- src/index.js
+++ src/index.js
@@ -1,48 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   let str1 = str.split("");
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i] == "[") {
-      for (let j = 0; j < str1.length - i; j++) {
-        if (str1[i] !== "]");
-        return false;
-      }
-    }
-  }
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i] == "(") {
-      for (let j = 0; j < str1.length - i; j++) {
-        if (str1[i] !== ")");
-        return false;
-      }
-    }
-  }
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i] == "{") {
-      for (let j = 0; j < str1.length - i; j++) {
-        if (str1[i] !== "}");
-        return false;
-      }
-    }
-  }
-  /*for (let i = 0; i < str1.length; i++) {
-    if (str1[i] == "|") {
-      for (let j = 0; j < str1.length - i; j++) {
-        if (str1[i] !== "|");
-        return false;
-      }
-    }
-  }*/
-  for (let i = 0; i < str1.length; i++) {
-    if (
-      str1[i] !== "[" &&
-      str1[i] !== "{" &&
-      str1[i] !== "}" &&
-      str1[i] !== "]" &&
-      str1[i] !== ")" &&
-      str1[i] !== "(" &&
-      str1[i] !== "|"
-    ) {
-      return true;
-    }
+  let res1 = str1.filter((item) => item == "[");
+  let res2 = str1.filter((item) => item == "]");
+  let res3 = str1.filter((item) => item == "(");
+  let res4 = str1.filter((item) => item == ")");
+  let res5 = str1.filter((item) => item == "{");
+  let res6 = str1.filter((item) => item == "}");
+  if (
+    res1.length == res2.length &&
+    res3.length == res4.length &&
+    res5.length == res6.length
+  ) {
+    return true;
+  } else {
+    return false;
   }
 };
