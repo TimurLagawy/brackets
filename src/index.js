5c420119357ac9d44f495f3040530de11f15d9b2
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-17 16:15:02 +0200

    fix: fix some code & pass sone tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   let str1 = str.split("");
   for (let i = 0; i < str1.length; i++) {
+    if (str1[i] == "[") {
+      for (let j = 0; j < str1.length - i; j++) {
+        if (str1[i] !== "]");
+        return false;
+      }
+    }
+  }
+  for (let i = 0; i < str1.length; i++) {
     if (
-      str1[i] != "[" &&
-      str1[i] != "{" &&
-      str1[i] != "}" &&
-      str1[i] != "]" &&
-      str1[i] != ")" &&
-      str1[i] != "(" &&
-      str1[i] != "|"
+      str1[i] !== "[" &&
+      str1[i] !== "{" &&
+      str1[i] !== "}" &&
+      str1[i] !== "]" &&
+      str1[i] !== ")" &&
+      str1[i] !== "(" &&
+      str1[i] !== "|"
     ) {
       return true;
     }
